a89d677ed616bab188da5b38b7fdfb83e5054608
Sumit Banik <sumit@apenning.xyz>
2021-09-08 09:18:53 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aut voluptatem non commodi dignissimos recusandae voluptatibus praesentium! Pariatur qui veritatis aperiam iusto aut. Consequuntur magni sapiente quisquam consectetur dolores eligendi omnis facere exercitationem temporibus!</p>
+        </div>
+    )
+}
